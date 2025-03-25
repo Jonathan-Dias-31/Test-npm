16e483eeeb5266cb8ddf46b10fc9f00f4e48ec53
JONATHAN DIAS <jonathan-dias@outlook.fr>
2025-03-25 12:11:35 +0100

    First try with npm

---
+++ script.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello Annick'));
+console.log(chalk.red('Hello Ridouane'));
+console.log(chalk.yellow('Hello Camille'));
+console.log(chalk.green('Hello Jérôme'));
+console.log(chalk.magenta('Hello Clément'));
